8c0417f73dbc4d4caa7aaa4dc6fe4bceceec6eb0
Mariatroyano <maria2020troyano@gmail.com>
2024-09-09 15:48:10 -0500

    se agrga lo ultimo que ise de la validacion

--- ValidacionEdad/Component/Formulario/Formulario.jsx
+++ ValidacionEdad/Component/Formulario/Formulario.jsx
@@ -1,35 +1,90 @@
 import React, { useState } from "react";
-import Modal from "../Formulario/modal/Modal";
 import "./formulario.css";
 
 const Formulario = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [age, setAge] = useState("");
+  const [edad, setEdad] = useState("");
+  const [validacionMensaje, setValidacionMensaje] = useState("");
+  const [array, setArray] = useState([]);
 
-  const handleOpenModal = () => setIsModalOpen(true);
+  const validacion = (edadNumero) => {
+    if (edadNumero > 50) {
+      setValidacionMensaje("estas viejo");
+      return
+    }
 
-  const edadNumero = parseInt(age);
-  let validacionMensaje = "";
+     if (edadNumero > 18) {
+      setValidacionMensaje("Puedes ingresar al evento");
+      setArray([...array, objetoFecha])
+      return
+    } 
 
-  if (edadNumero > 18) {
-    validacionMensaje = "Puedes ingresar al evento";
-  } else if (edadNumero >= 12 && edadNumero <= 18) {
-    validacionMensaje =
-      "Puedes ingresar al evento con supervisión de un adulto";
-  } else {
-    validacionMensaje = "No puedes ingresar al evento";
-  }
+     if (edadNumero >= 12 && edadNumero <= 18) {
+      setValidacionMensaje(
+        "Puedes ingresar al evento con supervisión de un adulto"
+      );
+      return
+    } 
+      setValidacionMensaje("No puedes ingresar al evento");
+    
+  };
+
+
+
+  const validar = (edades) => {
+    edades.preventDefault();
+
+    const edadNumero = parseInt(edad);
+    if (!isNaN(edadNumero)) {
+      validacion(edadNumero);
+    } else {
+      setValidacionMensaje("Por favor, ingresa un número válido.");
+    }
+  };
+
+  const fechaActual = new Date();
+
+  const objetoFecha = {
+    id: crypto.randomUUID(),
+    fecha: fechaActual.toLocaleDateString(),
+    hora: fechaActual.toLocaleTimeString(),
+  };
 
   return (
     <div>
       <div className="contenedorPrincipal">
-        <button onClick={handleOpenModal} className="open-modal-btn">
-          Abrir Modal
-        </button>
-        <h2 className="Titulo">Registros de Validación de edad para ingreso al evento:</h2>
-      </div>
-      <div>
-        <Modal />
+        <h2 className="Titulo">
+          Registros de Validación de edad para ingreso al evento:
+        </h2>
+        <div className="contenedorSecundario">
+          <div className="modal">
+            <h2>Ingresa tu edad</h2>
+            <form onSubmit={validar}>
+              <label htmlFor="edad">Edad:</label>
+              <input
+                type="text"
+                placeholder="Ingresa tu edad"
+                value={edad}
+                onChange={(e) => setEdad(e.target.value)}
+              />
+              <button onClick={setArray} type="submit">
+                Validar
+              </button>
+            </form>
+
+            <div>
+              {/* {setArray . setArray}
+              {array . array} */}
+              <p>{objetoFecha.id}</p>
+              <p>{objetoFecha.fecha}</p>
+              <p>{objetoFecha.hora}</p>
+            </div>
+            {validacionMensaje && <p>{validacionMensaje}</p>}
+            {/* <div>
+              setArray={setArray}
+              array={array}
+            </div> */}
+          </div>
+        </div>
       </div>
     </div>
   );
